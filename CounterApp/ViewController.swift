6b338e88dcd21a8a4c4f62ffc0ebdfec3cc3db3a
Ansh Kataria <anshkataria710@gmail.com>
2025-04-05 21:53:31 +0530

    Initial Commit

---
+++ CounterApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CounterApp
+//
+//  Created by Ansh Kataria on 05/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
